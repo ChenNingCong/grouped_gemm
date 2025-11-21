b8045360be4dd27108e2341391296822b79ac2f2
Zhiyang-Z <zhiyang_zhang@hotmail.com>
2025-11-21 16:02:50 -0500

    Register ops into pytorch and add fake tensor implementation

--- grouped_gemm/backend.py
+++ grouped_gemm/backend.py
@@ -7,9 +7,60 @@ import torch
 # c++ operations.
 import grouped_gemm_backend as backend
 
-# register them intro graphs
-torch.compiler.allow_in_graph(backend.gmm_sm89)
-torch.compiler.allow_in_graph(backend.gmm)
+# # register them intro graphs
+# torch.compiler.allow_in_graph(backend.gmm_sm89)
+# torch.compiler.allow_in_graph(backend.gmm)
+@torch.library.custom_op("grouped_gemm_backend::gmm", mutates_args={"c"}, device_types="cuda")
+def gmm_handler(a : torch.Tensor, 
+                b : torch.Tensor, 
+                c : torch.Tensor, 
+                batch_sizes : torch.Tensor, 
+                trans_a : bool, 
+                trans_b : bool) -> torch.Tensor:
+    return backend.gmm(a, b, c, batch_sizes, trans_a, trans_b)
+
+@torch.library.custom_op("grouped_gemm_backend::gmm_sm89", mutates_args={"c"}, device_types="cuda")
+def gmm_sm89_handler(a : torch.Tensor, 
+                b : torch.Tensor, 
+                c : torch.Tensor, 
+                batch_sizes : torch.Tensor, 
+                trans_a : bool, 
+                trans_b : bool) -> torch.Tensor:
+    return backend.gmm_sm89(a, b, c, batch_sizes, trans_a, trans_b)
+
+def _(a : torch.Tensor, 
+        b : torch.Tensor, 
+        c : torch.Tensor, 
+        batch_sizes : torch.Tensor, 
+        trans_a : bool, 
+        trans_b : bool) -> torch.Tensor:
+    if not trans_a and trans_b:
+        # forward pass
+        assert a.ndim == 2
+        assert b.ndim == 3
+        assert a.shape[-1] == b.shape[-1]
+        return torch.empty(a.shape[0], b.shape[1])
+    elif trans_a and not trans_b:
+        # a : [num_tokens, hidden_out]
+        # b : [num_tokens, hidden_in]
+        # c : [num_experts, hidden_out, hidden_in]
+        assert a.ndim == 2
+        assert b.ndim == 2
+        assert a.shape[0] == b.shape[0]
+        return torch.empty(batch_sizes.shape[0], a.shape[-1], b.shape[-1])
+    elif not trans_a and not trans_b:
+        # a : [num_tokens, hidden_out]
+        # b : [num_expert, hidden_out, hidden_in]
+        # c : [num_tokens, hidden_in]
+        assert a.ndim == 2
+        assert b.ndim == 3
+        assert a.shape[1] == b.shape[1]
+        return torch.empty(a.shape[0], b.shape[-1])
+    else:
+        assert False
+
+gmm_handler.register_fake(_)
+gmm_sm89_handler.register_fake(_)
 
 def _allocate_output(a, b, batch_sizes, trans_a, trans_b):
     assert not (trans_a and trans_b)
@@ -27,13 +78,13 @@ def _allocate_output(a, b, batch_sizes, trans_a, trans_b):
 # Assuming 'backend' and '_allocate_output' are available
 
 def gmm(a : torch.Tensor, b, batch_sizes, trans_a=False, trans_b=False, c=None):
-    gmm_func = backend.gmm
+    gmm_func = torch.ops.grouped_gemm_backend.gmm
 
     if a.is_cuda:
         major, minor = torch.cuda.get_device_capability(a.get_device())
         
         if major == 8 and minor == 9:
-            gmm_func = backend.gmm_sm89
+            gmm_func = torch.ops.grouped_gemm_backend.gmm_sm89
 
     if c is None:
         c = _allocate_output(a, b, batch_sizes, trans_a, trans_b)
